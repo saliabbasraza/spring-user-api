9865cd900c60dd81982c26a76167a0bd395bef30
saliabbasraza <s.ali.abbas.raza@gmail.com>
2020-03-28 00:04:30 +0500

    Initial commit

---
+++ src/test/java/com/basho/springuserapi/SpringUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basho.springuserapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
